0702361e031b1aba6d90b53266ba2a7d2fedd9fe
Muhammad Moiz <muhammadmoiz0087@gmail.com>
2022-02-28 14:13:39 +0500

    added a change

--- src/container/Books/ViewBook/ViewBook.jsx
+++ src/container/Books/ViewBook/ViewBook.jsx
@@ -47,6 +47,7 @@ const ViewBook = () => {
         data: currentBook,
       })
     );
+    setCurrentBook({});
     onClose();
   };
 
--- src/container/Student/ViewStudent/ViewStudent.jsx
+++ src/container/Student/ViewStudent/ViewStudent.jsx
@@ -46,6 +46,7 @@ const ViewStudent = () => {
         data: currentStudent,
       })
     );
+    setCurrentStudent({});
     onClose();
   };
 
